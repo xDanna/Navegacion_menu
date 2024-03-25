ce3cf3701d1e0d155afe4f5e60809a8201c1d8a0
Ernesto <ernesto@Ernesto-Imac.local>
2024-03-25 14:19:41 -0400

    Añadidos menus y submenus, imagenes de cervezas

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(){
 //EMPIEZA ACÀ
+    const submenu = document.querySelector(".submenu");
+    const tieneSubmenu = submenu.parentNode;
+    const grupoSubmenus = Array.from(tieneSubmenu);
+    console.log(grupoSubmenus);
+    
+    grupoSubmenus.forEach((item) =>{
+        let icono = this.querySelector("span");
+        item.addEventListener("mouseover", () =>{
+            icono.classList.remove("caret_down");
+            icono.classList.add("caret_up");
 
+        });
+        item.addEventListener("mouseleave", () =>{
+            icono.classList.remove("caret_up");
+            icono.classList.add("caret_down");
+            
+
+        });
+    });
+
+
+   
 
 });
