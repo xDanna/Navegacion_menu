3dc7308d830c4ca3f74c0987a5d001b36f116c1a
danna <Niccolastrres16@gmail.com>
2024-04-01 20:55:43 -0400

    si

--- js/funciones.js
+++ js/funciones.js
@@ -1,25 +1,14 @@
+
 document.addEventListener("DOMContentLoaded", function(){
 //EMPIEZA ACÀ
-    const submenu = document.querySelector(".submenu");
-    const tieneSubmenu = submenu.parentNode;
-    const grupoSubmenus = Array.from(tieneSubmenu);
-    console.log(grupoSubmenus);
-    
-    grupoSubmenus.forEach((item) =>{
-        let icono = this.querySelector("span");
-        item.addEventListener("mouseover", () =>{
-            icono.classList.remove("caret_down");
-            icono.classList.add("caret_up");
-
-        });
-        item.addEventListener("mouseleave", () =>{
-            icono.classList.remove("caret_up");
-            icono.classList.add("caret_down");
-            
-
-        });
+    let glide = new Glide(".glide",{
+        type: 'carousel',
+        perView: 4,
+        gap:10,
+        autoplay:3000,
+        hoverpause: true
     });
-
+glide.mount();
 
    
 
