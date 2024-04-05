151fcce6228c21199579bb78130cc081e4afd871
danna <Niccolastrres16@gmail.com>
2024-04-05 01:24:46 -0400

    archivo mejorado

--- js/funciones.js
+++ js/funciones.js
@@ -3,13 +3,11 @@ document.addEventListener("DOMContentLoaded", function(){
 //EMPIEZA ACÀ
     let glide = new Glide(".glide",{
         type: 'carousel',
-        perView: 4,
-        gap:10,
+        perView: 3,
+        gap:3,
         autoplay:3000,
         hoverpause: true
     });
 glide.mount();
-
-   
 
 });
